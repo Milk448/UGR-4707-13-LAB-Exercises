57bb15c740c248f3e39923c889cf80c46f519a1f
JMO <150449036+jiroomi@users.noreply.github.com>
2024-04-08 10:00:21 -0700

    navigator1.0

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:currency_convert/exercise_1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'navigation',
+      home: ScreenOne(),
+    );
+  }
+}
