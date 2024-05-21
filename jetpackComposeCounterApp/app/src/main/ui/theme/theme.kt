9c9fa77ce59de93cc282c8c8215226066a7fd06f
jiroomi <jiroomilkoo@gmail.com>
2024-05-21 21:34:05 +0300

    push jetcompose project

---
+++ jetpackComposeCounterApp/app/src/main/ui/theme/theme.kt
@@ -0,0 +1,37 @@
+package com.example.composeproject.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun ComposeProjectTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
---
+++ jetpackComposeCounterApp/app/src/main/ui/theme/type.kt
@@ -0,0 +1,13 @@
+package com.example.composeproject.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.FontFamily
+
+val Typography = Typography(
+    body1 = androidx.compose.ui.text.TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = androidx.compose.ui.text.font.FontWeight.Normal,
+        fontSize = androidx.compose.ui.unit.sp(16.sp)
+    )
+    
+)
